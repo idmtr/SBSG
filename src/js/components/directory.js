ade96944e658b388ca40ac6de5160ffbcd7d029c
IDMTR <mail@idmtr.com>
2018-09-28 17:15:49 +0200

    Fix directory big

--- src/js/components/directory.js
+++ src/js/components/directory.js
@@ -1,55 +1,55 @@
-    $(function() {
-        // 
-        $.get("https://directory.startupberlin.co/wp-json/wp/v2/resources", function(data) {
-            var LIMIT = 3;
-            var $directoryDiv = $('#directory-entries');
+$(function() {
+    // 
+    $.get("https://directory.startupberlin.co/wp-json/wp/v2/resources", function(data) {
+        var LIMIT = 3;
+        var $directoryDiv = $('#directory-entries');
 
-            $directoryDiv.empty();
+        $directoryDiv.empty();
 
-            for (var i = 0; i < LIMIT; ++i) {
-                var item = data[i];
-                var $newDiv = $('<div class="white-box wb2"><a href="#" class="entry-link"><span class="entry-text"></span><img src="placeholder.gif" class="entry-img"></a></div>');
+        for (var i = 0; i < LIMIT; ++i) {
+            var item = data[i];
+            var $newDiv = $('<div class="white-box wb2"><a href="#" class="entry-link"><span class="entry-text"></span><img src="placeholder.gif" class="entry-img"></a></div>');
 
-                // console.log(item);
+            // console.log(item);
 
-                $newDiv.find('.entry-link')
-                    .attr('href', item.link);
-                $newDiv.find('.entry-text')
-                    .text(item.title.rendered);
+            $newDiv.find('.entry-link')
+                .attr('href', item.link);
+            $newDiv.find('.entry-text')
+                .text(item.title.rendered);
 
-                var featuredMedia = null;
-                try {
-                    featuredMedia = item._links['wp:featuredmedia'][0].href;
-                } catch (e) {}
+            var featuredMedia = null;
+            try {
+                featuredMedia = item._links['wp:featuredmedia'][0].href;
+            } catch (e) {}
 
-                if (featuredMedia) {
+            if (featuredMedia) {
 
-                    (function($newDiv) {
+                (function($newDiv) {
 
-                        $.get(featuredMedia, function(fmData) {
-                            //console.log(fmData);
-                            var sizes = ['medium', 'thumbnail', 'chipmunk-sm'];
-                            var medias = fmData.media_details.sizes;
-                            var thumbnailUrl;
+                    $.get(featuredMedia, function(fmData) {
+                        //console.log(fmData);
+                        var sizes = ['medium', 'thumbnail', 'chipmunk-sm'];
+                        var medias = fmData.media_details.sizes;
+                        var thumbnailUrl;
 
-                            for (var i = 0; i < sizes.length; ++i) {
-                                var size = sizes[i];
-                                if (medias[size] && medias[size].source_url) {
-                                    thumbnailUrl = medias[size].source_url;
-                                    break;
-                                }
+                        for (var i = 0; i < sizes.length; ++i) {
+                            var size = sizes[i];
+                            if (medias[size] && medias[size].source_url) {
+                                thumbnailUrl = medias[size].source_url;
+                                break;
                             }
-                            if (thumbnailUrl) {
-                                $newDiv.find('.entry-img')
-                                    .attr('src', thumbnailUrl)
-                                    .attr('alt', fmData.alt_text);
-                            }
-                        });
+                        }
+                        if (thumbnailUrl) {
+                            $newDiv.find('.entry-img')
+                                .attr('src', thumbnailUrl)
+                                .attr('alt', fmData.alt_text);
+                        }
+                    });
 
-                    })($newDiv);
+                })($newDiv);
 
-                }
-                $directoryDiv.append($newDiv);
             }
-        });
-    })
+            $directoryDiv.append($newDiv);
+        }
+    });
+})
